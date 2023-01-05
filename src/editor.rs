5bef040af72279ae3a9c8eb62753696bd6d416e3
sussy baka <example@example.com>
2023-01-04 22:24:54 -0600

    broken image background

--- src/editor.rs
+++ src/editor.rs
@@ -1,10 +1,54 @@
 use nih_plug::prelude::Editor;
-use nih_plug_vizia::vizia::prelude::*;
 use nih_plug_vizia::widgets::*;
 use nih_plug_vizia::{assets, create_vizia_editor, ViziaState, ViziaTheming};
+
+use nih_plug_vizia::vizia::*;
+use nih_plug_vizia::vizia::prelude::*;
+use nih_plug_vizia::vizia::vg::ImageId;
+use nih_plug_vizia::vizia::vg::RenderTarget;
+use nih_plug_vizia::vizia::vg::{Paint, Path};
+use nih_plug_vizia::vizia::vg::ImageFlags;
+
+use std::cell::RefCell;
 use std::sync::Arc;
+use std::rc::Rc;
 
 use crate::BassParams;
+
+struct BackgroundImage {
+    image: Rc<RefCell<Option<ImageId>>>,
+}
+
+impl BackgroundImage {
+    pub fn new(cx: &mut Context) -> Handle<Self> {
+        Self {
+            image: Rc::new(RefCell::new(None)),
+        }
+        .build(cx, |_| {})
+    }
+}
+
+impl View for BackgroundImage {
+    fn draw(&self, cx: &mut DrawContext<'_>, canvas: &mut Canvas) {
+        let bounds = cx.bounds();
+        let image_id = if let Some(image_id) = *self.image.borrow() {
+            image_id
+        } else {
+            canvas.load_image_mem(include_bytes!("gradient.png"), ImageFlags::GENERATE_MIPMAPS).expect("error loading background image")
+        };
+
+        *self.image.borrow_mut() = Some(image_id);
+        canvas.set_render_target(RenderTarget::Image(image_id));
+
+        let mut path = Path::new();
+        path.rect(bounds.x, bounds.y, bounds.w, bounds.h);
+
+        canvas.fill_path(
+            &mut path,
+            &Paint::image(image_id, bounds.x, bounds.y, bounds.w, bounds.h, 0.0, 1.0),
+        );
+    }
+}
 
 #[derive(Lens)]
 struct Data {
@@ -51,6 +95,8 @@ pub(crate) fn create(
 
             Label::new(cx, "output gain").bottom(Pixels(0.0));
             ParamSlider::new(cx, Data::params, |params| &params.output_gain);
+
+            //BackgroundImage::new(cx);
         })
         .row_between(Pixels(0.0))
         .child_left(Stretch(1.0))
